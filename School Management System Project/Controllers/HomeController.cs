b0d1690a7e4c37631a19f480e23c2a0789a5d706
SAHDEV RATHOD <sahdevrathod2020@gmail.com>
2026-04-23 22:05:43 +0530

    Student Added in View

--- School Management System Project/Controllers/HomeController.cs
+++ School Management System Project/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using School_Management_System_Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,13 @@ namespace School_Management_System_Project.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            List<Student> students = new List<Student>
+            {
+                new Student { Id = 1, FirstName = "John", LastName = "Doe", DateOfBirth = new DateTime(2000, 1, 1), Email = "SahdevRathod@gmail.com" },
+                new Student { Id = 2, FirstName = "Jane", LastName = "Smith", DateOfBirth = new DateTime(2001, 2, 2), Email = "JaneSmit@gmail.com", },
+                new Student { Id = 3, FirstName = "Michael", LastName = "Johnson", DateOfBirth = new DateTime(2002, 3, 3), Email = "Michael@gmail.com" }
+            };
+
             return View();
         }
     }
---
+++ School Management System Project/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School_Management_System_Project.Models
+{
+    /*
+    Pseudocode / Plan:
+    - Add five properties to the Student class to represent basic student data.
+    - Properties to add:
+      1. Id (int)                   : unique identifier for the student
+      2. FirstName (string)         : student's given name
+      3. LastName (string)          : student's family name
+      4. DateOfBirth (DateTime)     : student's birth date
+      5. Email (string)             : contact email for the student
+    - Each property should be public with standard get and set accessors.
+    - Keep implementation simple and POCO-style for compatibility with EF/serialization.
+    */
+
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; }
+    }
+}
